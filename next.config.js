67307351f87a6ce1498742835e03059030b9870e
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-20 00:25:55 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const REPO_NAME = 'stripe-doc';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? '/stripe-doc' : '',
+  basePath: process.env.GITHUB_ACTIONS ? '/' + REPO_NAME : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/' + REPO_NAME : '',
   trailingSlash: true,
   webpack: (config) => {
     config.module.rules.push({
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,19 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" enableColorScheme={false}>
+      <Head>
+        <link
+          rel="icon"
+          href={`${
+            process.env.GITHUB_ACTIONS ? '/stripe-doc' : ''
+          }/favicon.ico`}
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
