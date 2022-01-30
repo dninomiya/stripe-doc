e0239c718a364549432081c3879b11702dce4a8d
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-30 16:44:58 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import Layout from '../components/layout';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
+
+const SITE_TITLE = 'Stripeビギナーズガイド';
+const SITE_URL = 'https://flock-team.github.io/stripe-doc';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,6 +20,22 @@ function MyApp({ Component, pageProps }: AppProps) {
           }/favicon.ico`}
         />
       </Head>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          url: 'https://flock-team.github.io/stripe-doc/',
+          site_name: SITE_TITLE,
+          images: [
+            {
+              url: SITE_URL + '/ogp.png',
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+        titleTemplate={`%s | ${SITE_TITLE}`}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
